40da2eca92c85917aab1392cc5413cf4e9e4dd74
John DeCuir <johndec@outlook.com>
2018-02-10 23:13:01 -0800

    Added gameboard logic

---
+++ DondSim/GameBoard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DondSim
+{
+    class GameBoard
+    {
+        public List<double> _board { get; } = new List<double>()
+        {
+            0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750,
+            1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000,
+            750000, 1000000
+        };
+
+        public double getAverage()
+        {
+            return _board.Average();
+        }
+
+        public void removeValue(double valueToRemove)
+        {
+            if (_board.Contains(valueToRemove))
+            {
+                _board.Remove(valueToRemove);
+            }
+            else throw new ArgumentException("value to be removed was not found");
+        }
+    }
+}
--- DondSim/Program.cs
+++ DondSim/Program.cs
@@ -7,6 +7,11 @@ namespace DondSim
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            GameBoard gb = new GameBoard();
+            Console.WriteLine("Average is {0}", gb.getAverage());
+            gb.removeValue(1000000);
+            Console.WriteLine("Average is {0}", gb.getAverage());
         }
     }
 }
